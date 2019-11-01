7345bd3a6e1689db29931e3ec73cd731c503fd6f
JH <1801397@sit.singaporetech.edu.sg>
2019-11-01 10:20:56 +0800

    fff

--- servo_pwm_motor.c
+++ servo_pwm_motor.c
@@ -1,47 +1,41 @@
-/* DriverLib Includes */
+#include <stdbool.h>
+#include <stdint.h>
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
-/* Standard Includes */
-#include <stdint.h>
-#include <stdbool.h>
 
-int a = 0;
+//***** Definitions *****
+#define CPU_FREQ                    (24000000)
 
+//***** Function Prototypes *****
+void startCrystalOscillator(void);
+void setSystemClock(uint32_t CPU_Frequency);
 
-/* Timer_A PWM Configuration Parameter */
+//***** Global Data *****
+// Timer_A PWM Configuration Parameter
 Timer_A_PWMConfig pwmConfig =
 {
-        //uint_fast16_t clockSource;
         TIMER_A_CLOCKSOURCE_SMCLK,
-        //uint_fast16_t clockSourceDivider;
         TIMER_A_CLOCKSOURCE_DIVIDER_1,
-        //uint_fast16_t timerPeriod;
-        12800,
-        //uint_fast16_t compareRegister;
-        TIMER_A_CAPTURECOMPARE_REGISTER_1,
-        //uint_fast16_t compareOutputMode;
-        TIMER_A_OUTPUTMODE_RESET_SET,
-        //uint_fast16_t dutyCycle;
-        100
+        11720,                        
+        TIMER_A_CAPTURECOMPARE_REGISTER_0,
+        TIMER_A_OUTPUTMODE_OUTBITVALUE /*TIMER_A_OUTPUTMODE_TOGGLE*/,
+        11720
 };
 
-// PWM - PIN - 5.6 - TIMER_A2_BASE
-// PWM - PIN - 2.4 - TIMER_A0_BASE
 
-int main(void)
+/***********************************************************
+  Function:
+*/
+void main(void)
 {
-    /* Halting the watchdog */
-    MAP_WDT_A_holdTimer();
+    WDT_A_holdTimer();
+    MAP_Interrupt_disableMaster();                // Disabling master interrupts
 
-    /* Setting MCLK to REFO at 128Khz for LF mode setting SMCLK to 64Khz */
-    MAP_CS_setReferenceOscillatorFrequency(CS_REFO_128KHZ);
-    MAP_CS_initClockSignal(CS_MCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
-    MAP_CS_initClockSignal(CS_SMCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_2);
-    MAP_PCM_setPowerState(PCM_AM_LF_VCORE0);
+    // Configure clocks
+    startCrystalOscillator();
+    setSystemClock(CPU_FREQ);
 
-    // Configuring GPIO2.4 as peripheral output for PWM
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN4, GPIO_PRIMARY_MODULE_FUNCTION);
-
+    // P1.1 for button interrupt
     MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
     MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN1);
     MAP_GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
@@ -50,23 +44,106 @@ int main(void)
     MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN4);
     MAP_GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN4);
 
-    //MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig);
+
+    // P1.0 as output
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+
+    // Output MCLK and ACLK
+//  MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P4,
+//          GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    // Timer A0
+    // Set the Period for CC0
+    MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig);
+
+    // Configuring GPIO 2.4 as peripheral output for PWM - TA0.1
+    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN4,
+            GPIO_PRIMARY_MODULE_FUNCTION);
+
+    // Set the Duty Cycle for CC1 - 10%
+    pwmConfig.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_1;
+    pwmConfig.dutyCycle = 2344;
+    pwmConfig.compareOutputMode = TIMER_A_OUTPUTMODE_RESET_SET;
+    MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig);
+
 
     MAP_Interrupt_enableInterrupt(INT_PORT1);
     MAP_Interrupt_enableSleepOnIsrExit();
     MAP_Interrupt_enableMaster();
 
-    pwmConfig.dutyCycle = 60;
-    MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig);
+    static volatile uint32_t aclk, mclk, smclk, hsmclk, bclk;
 
+    aclk = CS_getACLK();
+    mclk = CS_getMCLK();
+    smclk = CS_getSMCLK();
+    hsmclk = CS_getHSMCLK();
+    bclk = CS_getBCLK();
+
+    /* Sleeping when not in use */
     while (1)
     {
         MAP_PCM_gotoLPM0();
     }
-    // Configuring Timer_A to have a period of approximately 20ms
-    //MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig);
 }
 
+/***********************************************************
+  Function:
+
+   The following function is responsible for starting XT1 in the
+   MSP432 that is used to source the internal FLL that drives the
+   MCLK and SMCLK.
+*/
+void startCrystalOscillator(void)
+{
+    /* Configuring pins for peripheral/crystal HFXT*/
+    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_PJ,
+            GPIO_PIN3 | GPIO_PIN4, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Configuring pins for peripheral/crystal LFXT*/
+    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_PJ,
+            GPIO_PIN0 | GPIO_PIN1, GPIO_PRIMARY_MODULE_FUNCTION);
+}
+
+/***********************************************************
+  Function:
+
+   The following function is responsible for setting up the system
+   clock at a specified frequency.
+*/
+void setSystemClock(uint32_t CPU_Frequency)
+{
+    /* Setting the external clock frequency. This API is optional, but will
+     * come in handy if the user ever wants to use the getMCLK/getACLK/etc
+     * functions
+     */
+    MAP_CS_setExternalClockSourceFrequency(32768, CPU_Frequency);
+
+    /* Before we start we have to change VCORE to 1 to support the 48MHz frequency */
+    MAP_PCM_setCoreVoltageLevel(PCM_AM_LDO_VCORE1);
+    MAP_FlashCtl_setWaitState(FLASH_BANK0, 2);
+    MAP_FlashCtl_setWaitState(FLASH_BANK1, 2);
+
+    /* Starting HFXT and LFXT in non-bypass mode without a timeout. */
+    MAP_CS_startHFXT(false);
+    MAP_CS_startLFXT(false);
+
+    /* Initializing the clock source as follows:
+     *      MCLK = HFXT = 48MHz
+     *      ACLK = LFXT = 32KHz
+     *      HSMCLK = HFXT/4 = 12MHz
+     *      SMCLK = HFXT/8 = 6MHz
+     *      BCLK  = REFO = 32kHz
+     */
+    MAP_CS_initClockSignal(CS_MCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    MAP_CS_initClockSignal(CS_ACLK, CS_LFXTCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    MAP_CS_initClockSignal(CS_HSMCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_4);
+    MAP_CS_initClockSignal(CS_SMCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_8);
+    MAP_CS_initClockSignal(CS_BCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+}
+
+/***********************************************************
+  Function: port1IntHandler
+ */
 void PORT1_IRQHandler(void)
 {
     uint32_t status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P1);
@@ -74,13 +151,27 @@ void PORT1_IRQHandler(void)
 
     if (status & GPIO_PIN1)
     {
-        pwmConfig.dutyCycle = 40;
+        // Timer A1
+        if(pwmConfig.dutyCycle >= 10548)
+        {
+            pwmConfig.dutyCycle = 3000;
+        }
+        else
+        {
+            pwmConfig.dutyCycle += 3000;
+        }
         MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig);
     }
-    else if(status & GPIO_PIN4){
-        pwmConfig.dutyCycle = 140;
+
+    if (status & GPIO_PIN4){
+
+        if(pwmConfig.dutyCycle <= 0){
+            pwmConfig.dutyCycle = 3000;
+        }
+        else
+        {
+            pwmConfig.dutyCycle -= 3000;
+        }
         MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig);
     }
 }
-
-
